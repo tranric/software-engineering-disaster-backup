7dc19b3b9c38aeba7b2b35c8cdddecd627e638df
ArseniiMitianin <arsenii.mitianin@yandex.ru>
2019-12-03 20:41:52 -0500

    For some reason, the logout servlet didn't update.

--- src/carregistry/controllers/ProcessLogout.java
+++ src/carregistry/controllers/ProcessLogout.java
@@ -11,20 +11,17 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/ProcessLogout")
 public class ProcessLogout extends HttpServlet {
-
-
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		session.removeAttribute("Role");
 		session.removeAttribute("Id");
 		session.removeAttribute("Login");
 		session.invalidate();
+		
 		response.sendRedirect("LoginPage.jsp");
 	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-
 }
